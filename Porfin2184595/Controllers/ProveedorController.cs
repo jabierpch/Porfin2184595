c846f59ca0d39b7ebcfd990d42cbcac4480a4375
JABIER PRIETO <jfprieto37@misena.edu.co>
2021-06-18 14:33:40 -0500

    Creación Controllers cliente, producto, proveedor, rolex y los Viems create, dentails, edit e Index de los mismos.

---
+++ Porfin2184595/Controllers/ProveedorController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Porfin2184595.Models;
+
+namespace Porfin2184595.Controllers
+{
+    public class ProveedorController : Controller
+    {
+        // GET: Proveedor
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+
+                return View(db.proveedor.ToList());
+            }
+        }
+        public static string NombreProveedor(int idProveedor)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.proveedor.Find(idProveedor).nombre;
+            }
+        }
+        public ActionResult Create()
+
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(proveedor proveedor)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.proveedor.Add(proveedor);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception Ex)
+            {
+                ModelState.AddModelError("", "error" + Ex);
+                return View();
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    proveedor findUser = db.proveedor.Where(a => a.id == id).FirstOrDefault();
+                    return View(findUser);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(proveedor proveedorEdit)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    proveedor user = db.proveedor.Find(proveedorEdit.id);
+                    user.nombre = proveedorEdit.nombre;
+                    user.direccion = proveedorEdit.direccion;
+                    user.telefono = proveedorEdit.telefono;
+                    user.nombre_contacto = proveedorEdit.nombre_contacto;
+
+
+                    db.SaveChanges();
+                    return RedirectToAction("index");
+                }
+            }
+            catch (Exception ex)
+
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                proveedor user = db.proveedor.Find(id);
+                return View(user);
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            using (inventario2021Entities db = new inventario2021Entities())
+            {
+                var Usuario = db.proveedor.Find(id);
+                db.proveedor.Remove(Usuario);
+                db.SaveChanges();
+                return RedirectToAction("index");
+            }
+        }
+    }
+}
