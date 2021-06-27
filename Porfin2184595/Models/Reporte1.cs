e2452453f5c1a6a884aec7d7652a2611cd404dac
JABIER PRIETO <jfprieto37@misena.edu.co>
2021-06-26 22:57:05 -0500

    Reportes

---
+++ Porfin2184595/Models/Reporte1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Porfin2184595.Models
+{
+    public class Reporte1
+    {
+        public String nombreproducto { get; set; }
+        public String descripcionproducto { get; set; }
+        public int cantidadproducto { get; set; }
+        public String nombrecliente { get; set; }
+        public String emailcliente { get; set; }
+        public String documentocliente { get; set; }
+
+    }
+}
